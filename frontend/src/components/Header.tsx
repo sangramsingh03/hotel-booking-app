b2f6f4e72110ba1eaa953afb936ad42444411a17
Sangram Singh Gour <sangramsinghgour21112001@gmail.com>
2025-01-17 03:04:54 +0530

    created header and footer for frontend

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -7,7 +7,15 @@ const Header = () => {
                 <span className="text-3xl text-white font-bold tracking-tight">
                     <Link to="/">Holidays.com</Link>
                 </span>
+                <span className="flex space-x-2">
+                    <Link to="/sign-in" className="flex bg-white items-center text-blue-600 px-3 font-bold hover:bg-gray-100"
+                    >
+                        Sign In
+                    </Link>
+                </span>
             </div>
         </div>
     );
 };
+
+export default Header;
