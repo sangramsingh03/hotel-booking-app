9594ca8cd2f3f9a7dc7058d441fb693fba190c02
Sangram Singh Gour <sangramsinghgour21112001@gmail.com>
2025-01-19 12:04:15 +0530

    created user registration form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,7 @@
-import { 
-        BrowserRouter as Router,
-        Route,
-        Routes,
-        Navigate, } 
+import { BrowserRouter as Router, Route, Routes, Navigate } 
 from 'react-router-dom'
 import Layout from './layouts/Layout'
+import Register from './pages/Register';
 
 const App = () => {
   return (
@@ -23,6 +20,14 @@ const App = () => {
           element={
             <Layout>
               <p>Search page</p>
+            </Layout>
+          } 
+        />
+        <Route 
+          path="/register" 
+          element={
+            <Layout>
+              <Register />
             </Layout>
           } 
         />
