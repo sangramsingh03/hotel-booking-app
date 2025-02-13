167105cd0e86019049248278676d2628e7b4af7f
Sangram Singh Gour <sangramsinghgour21112001@gmail.com>
2025-02-14 00:53:06 +0530

    implemented sign out feature

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -1,8 +1,8 @@
 import { useForm } from "react-hook-form";
-import { useMutation } from "react-query";
+import { useMutation, useQueryClient } from "react-query";
 import * as apiClient from "../api-client";
 import { useAppContext } from "../contexts/AppContext";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export type SignInFormData = {
     email: string;
@@ -12,11 +12,13 @@ export type SignInFormData = {
 const SignIn = () => {
     const { showToast } = useAppContext();
     const navigate = useNavigate();
+    const queryClient = useQueryClient();
     const { register, formState: { errors }, handleSubmit } = useForm<SignInFormData>();
 
     const mutation = useMutation(apiClient.signIn, {
         onSuccess: async () => {
             showToast( { message: "Sign in Successful", type: "SUCCESS" });
+            await queryClient.invalidateQueries("validateToken");
             navigate("/");
         },
         onError: (error: Error) => {
@@ -56,7 +58,15 @@ const SignIn = () => {
                     ></input>
                     {errors.password && (<span className="text-red-500">{errors.password.message}</span>)}
             </label>
-            <span>
+            <span className="flex items-center justify-between">
+                <span className="text-sm">
+                    Not Registered? 
+                    <Link className="underline" 
+                    to="/register"
+                    >
+                        Create an account here
+                    </Link>
+                </span>
                 <button 
                     type="submit"
                     className="bg-blue-600 text-white p-2 font-bold hover:bg-blue-500 text-xl"
