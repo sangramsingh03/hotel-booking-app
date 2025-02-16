eb17ba6b4ecfe6d9a3bad33d22cd80b578b7f9b7
Sangram Singh Gour <sangramsinghgour21112001@gmail.com>
2025-02-16 15:51:02 +0530

    automated e2e test for user sign in

---
+++ e2e-tests/tests/auth.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect } from '@playwright/test';
+
+const UI_URL = 'http://localhost:5173/';
+
+test('should allow the user to sign in', async ({ page }) => {
+  await page.goto(UI_URL);
+
+  await page.getByRole('link', { name: "Sign In" }).click();
+
+  await expect(page.getByRole("heading", { name: "Sign In" })).toBeVisible();
+
+  await page.locator("[name=email]").fill("1@1.com");
+  await page.locator("[name=password]").fill("password123");
+
+  await page.getByRole("button", { name: "Login" }).click();
+
+  await expect(page.getByText("Sign in Successful!")).toBeVisible();
+  await expect(page.getByRole("link", { name: "My Bookings" })).toBeVisible();
+  await expect(page.getByRole("link", { name: "My Hotels" })).toBeVisible();
+  await expect(page.getByRole("button", { name: "Sign Out" })).toBeVisible();
+});
+
