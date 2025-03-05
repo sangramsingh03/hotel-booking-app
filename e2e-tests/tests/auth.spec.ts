23f4ee76aca4e44610893660be819517451ca3d3
Sangram Singh Gour <sangramsinghgour21112001@gmail.com>
2025-03-06 00:49:01 +0530

    added e2e test for user to add a hotel

--- e2e-tests/tests/auth.spec.ts
+++ e2e-tests/tests/auth.spec.ts
@@ -14,7 +14,7 @@ test('should allow the user to sign in', async ({ page }) => {
 
   await page.getByRole("button", { name: "Login" }).click();
 
-  await expect(page.getByText("Sign in Successful!")).toBeVisible();
+  await expect(page.getByText("Sign-in complete. Welcome back!")).toBeVisible();
   await expect(page.getByRole("link", { name: "My Bookings" })).toBeVisible();
   await expect(page.getByRole("link", { name: "My Hotels" })).toBeVisible();
   await expect(page.getByRole("button", { name: "Sign Out" })).toBeVisible();
