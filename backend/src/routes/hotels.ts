fa02c02fc2168b434d439c17eb30e99208e5d954
Sangram Singh Gour <sangramsinghgour21112001@gmail.com>
2025-03-30 15:49:30 +0530

    added property rating in search UI page

--- backend/src/routes/hotels.ts
+++ backend/src/routes/hotels.ts
@@ -7,13 +7,29 @@ const router = express.Router();
 router.get("/search", async (req: Request, res: Response) => {
     try{
         const query = constructSearchQuery(req.query);
+
+        let sortOptions = {};
+        switch(req.query.sortOption){
+          case "starRating":
+            sortOptions = { starRating : -1};
+            break;
+          case "pricePerNightAsc":
+            sortOptions = { pricePerNight : 1};
+            break;
+          case "pricePerNightDesc":
+            sortOptions = { pricePerNight : -1};
+            break;
+        }
         const pageSize = 5;
         const pageNumber = parseInt(
             req.query.page ? req.query.page.toString() : "1"
         );
         const skip = (pageNumber - 1) * pageSize;
 
-        const hotels = await Hotel.find(query).skip(skip).limit(pageSize);
+        const hotels = await Hotel.find(query)
+          .sort(sortOptions)
+          .skip(skip)
+          .limit(pageSize);
         const total = await Hotel.countDocuments(query);
 
         const response: HotelSearchResponse = {
@@ -53,7 +69,33 @@ const constructSearchQuery = (queryParams: any) => {
         $gte: parseInt(queryParams.childCount),
       };
     }
-    
+
+    if(queryParams.facilities){
+      constructedQuery.facilities = {
+        $all: Array.isArray(queryParams.facilities) ? queryParams.facilities : [queryParams.facilities]
+      };
+    }
+
+    if(queryParams.types){
+      constructedQuery.type = {
+        $in: Array.isArray(queryParams.types) ? queryParams.types : [queryParams.types]
+      };
+    }
+
+    if (queryParams.stars) {
+      const starRatings = Array.isArray(queryParams.stars)
+        ? queryParams.stars.map((star: string) => parseInt(star))
+        : parseInt(queryParams.stars);
+
+      constructedQuery.starRating = { $in: starRatings };
+    }
+
+    if(queryParams.maxPrice){
+      constructedQuery.pricePerNight = {
+        $lte : parseInt(queryParams.maxPrice.toString()),
+      }
+    }
+
     return constructedQuery;
 };
 
