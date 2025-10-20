796049cfdba96ff8e502ec5538a3eb1a595ab13a
Sangram Singh Gour <sangramsinghgour21112001@gmail.com>
2025-10-20 14:10:13 +0530

    fixed

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -6,11 +6,17 @@ const Header = () => {
     const { isLoggedIn } = useAppContext();
     return (
         <div className="bg-blue-800 py-6">
-            <div className="container mx-auto flex flex-col gap-2 sm:flex-row items-center sm:justify-between">
-                <span className="text-3xl text-white font-bold tracking-tight">
-                    <Link to="/">Holidays.com</Link>
-                </span>
-                <span className="flex gap-3">
+            <div className="container mx-auto flex flex-col gap-2 sm:gap-0 sm:flex-row items-center sm:justify-between">
+                <div className='flex justify-between items-center w-full'>
+                    <Link to="/" className='text-2xl sm:text-3xl text-white font-bold'>Holidays.com</Link>
+                    {!isLoggedIn && (
+                        <Link to="/sign-in" className="sm:hidden bg-white text-blue-600 m-0 hover:bg-gray-100 rounded-[1rem] p-2 text-[1rem] font-bold"
+                        >
+                            Sign in
+                        </Link>
+                    )}
+                </div>
+                <div className="flex gap-3 sm:justify-end w-full">
                     {isLoggedIn ? (
                         <>
                             <Link className="flex items-center text-white m-0 p-2 font-bold hover:bg-blue-600 rounded-[1rem]"
@@ -26,12 +32,12 @@ const Header = () => {
                             <SignOutButton />
                         </>
                     ) : (
-                        <Link to="/sign-in" className="flex bg-white items-center text-blue-600 p-2 m-0 font-bold hover:bg-gray-100 rounded-[1rem]"
+                        <Link to="/sign-in" className="hidden sm:flex bg-white items-center text-blue-600 p-2 m-0 font-bold hover:bg-gray-100 rounded-[1rem]"
                         >
                         Sign in
                         </Link>
                     )}
-                </span>
+                </div>
             </div>
         </div>
     );
